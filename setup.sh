a4d5cb15605291a5094c76e85b487329cf573109
code2344 <71059013+code2344@users.noreply.github.com>
2025-03-11 10:41:30 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,6 +27,19 @@ echo "Your public IP is: $(curl -s http://ipinfo.io/ip)"
 echo "Minecraft will be hosted on default port (25565)"
 echo "If you plan on playing with others, make sure to set up port forwarding on your router. "
 
+# Create the spigot-start command for future use
+echo "Creating spigot-start command..."
+cat <<EOF > /usr/local/bin/spigot-start
+#!/bin/bash
+cd /$(pwd) && java -Xms1G -Xmx1G -jar spigot-*.jar
+EOF
+
+# Make the spigot-start script executable
+chmod +x /usr/local/bin/spigot-start
+
+# Notify the user
+echo "spigot-start command has been created. You can now use it to start the server in the future."
+
 # Run the Spigot server
 echo "Starting Spigot server..."
 java -Xms1G -Xmx1G -jar spigot-*.jar
